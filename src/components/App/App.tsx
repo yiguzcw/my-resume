adc18366ccf11ddb724f54e586a2e61a1fea2e1c
yiguzcw <yiguzcw@163.com>
2019-04-30 01:11:29 +0800

    搭建页面框架

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,8 @@ import { ReactHTML } from 'react'
 import { IPayload } from '../../types'
 import Header from '../Layout/Header/Header'
 
+import Drogue from '../Layout/Drogue/Drogue'
+
 import './style.less'
 const { Content } = Layout
 
@@ -12,12 +14,22 @@ interface IProps {
   fetchArticle: (payload: IPayload) => void
 }
 class App extends React.Component<IProps> {
+  public state = {
+    headerMenu : [
+      { title: '基本资料', icon: { __html: '&#xe600;' }, url: '/' },
+      { title: '专业技能', icon: { __html: '&#xe660;' }, url: '/skill' },
+      { title: '工作经历', icon: { __html: '&#xe6a1;' }, url: '/experience' },
+      { title: '项目经验', icon: { __html: '&#xe60e;' }, url: '/project' },
+      { title: '自我评价', icon: { __html: '&#xe60e;' }, url: '/self-evaluation' }
+    ]
+  }
+
   public render() {
     const { children, fetchArticle } = this.props
     return (
       <Layout>
         <BackTop />
-        <Header fetchArticle={fetchArticle} />
+        <Header fetchArticle={fetchArticle} headerMenu={this.state.headerMenu}/>
         <Layout>
           <Content>
             <Row>
@@ -32,6 +44,8 @@ class App extends React.Component<IProps> {
             </Row>
           </Content>
         </Layout>
+        <Drogue headerMenu={this.state.headerMenu}/>
+        
       </Layout>
     )
   }
--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Col, Dropdown, Icon, Input, Layout, Menu, Row } from 'antd'
+import { Col, Dropdown, Icon, Layout, Menu, Row } from 'antd'
 import Texty from 'rc-texty'
 import 'rc-texty/assets/index.css'
 import TweenOne from 'rc-tween-one'
@@ -6,7 +6,6 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import './style.less'
 const { Header } = Layout
-const { Search } = Input
 const geInterval = (e: any) => {
   switch (e.index) {
     case 0:
@@ -35,30 +34,14 @@ const getEnter = (e: any) => {
   return t
 }
 
-const getSplit = (e: any) => {
-  const t = e.split(' ')
-  const c: any[] = []
-  t.forEach((str: string, i: number) => {
-    c.push(<span key={`${str}-${i}`}>{str}</span>)
-    if (i < t.length - 1) {
-      c.push(<span key={` -${i}`} />)
-    }
-  })
-  return c
-}
 interface IProps {
-  fetchArticle: (payload: object) => void
+  fetchArticle: (payload: object) => void,
+  headerMenu: any[]
 }
 const HeaderDom = (props: IProps) => {
-  const headerTitle = [
-    { title: '主页', icon: { __html: '&#xe600;' }, url: '/' },
-    { title: '归档', icon: { __html: '&#xe660;' }, url: '/time-file' },
-    { title: '说说', icon: { __html: '&#xe6a1;' }, url: '/say' },
-    { title: '收藏', icon: { __html: '&#xe60e;' }, url: '/collect' }
-  ]
   const menu = (
     <Menu className="menu">
-      {headerTitle.map(item => (
+      {props.headerMenu.map(item => (
         <Menu.Item key={item.title} className="header-title-item">
           <Link to={item.url}>
             <span
@@ -76,78 +59,48 @@ const HeaderDom = (props: IProps) => {
     <div className="header">
       <Header>
         <Row>
-          <Col md={0} lg={1} xl={3} xxl={5} />
-          <Col md={22} lg={20} xl={18} xxl={14}>
+          <Col md={24} lg={24} xl={24} xxl={24}>
             <div className="header-logo-wrp">
-              <div className="combined">
-                <Texty
-                  className="title"
-                  type="mask-top"
-                  delay={400}
-                  enter={getEnter}
-                  interval={geInterval}
-                  component={TweenOne}
-                  componentProps={{
-                    animation: [
-                      { x: 130, type: 'set' },
-                      { x: 100, delay: 500, duration: 450 },
-                      {
-                        duration: 300,
-                        ease: 'easeOutQuart',
-                        x: 0
-                      },
-                      {
-                        delay: -300,
-                        duration: 1000,
-                        ease: 'easeInOutQuint',
-                        letterSpacing: 0,
-                        scale: 0.9
-                      },
-                      {
-                        delay: -300,
-                        duration: 1000,
-                        ease: 'easeInOutQuint',
-                        scale: 1,
-                        width: '100%'
-                      }
-                    ]
-                  }}>
-                  Apple Blog
-                </Texty>
-                <TweenOne
-                  className="combined-bar"
-                  animation={{
-                    delay: 2000,
-                    ease: 'easeInOutExpo',
-                    type: 'from',
-                    width: 0,
-                    x: 158
-                  }}
-                />
-                <Texty
-                  className="content"
-                  type="bottom"
-                  split={getSplit}
-                  delay={2200}
-                  interval={30}>
-                  记录 自己 学习 历程，文章 写得 不好 请见 谅
-                </Texty>
-              </div>
-              <div className="menu-button-wrp">
-                <Dropdown overlay={menu} trigger={['click']}>
-                  <Icon type="bars" className="menu-button" />
-                </Dropdown>
-              </div>
+              <img src="images/my-head-portrait.jpg"/>
             </div>
-          </Col>
-        </Row>
-      </Header>
-      <Row className="header-footer">
-        <Col lg={1} xl={4} xxl={5} />
-        <Col lg={22} xl={18} xxl={14}>
-          <Row>
-            <Col xs={24} sm={24} md={17} lg={17} xl={17} xxl={17}>
-              {headerTitle.map(item => (
+            <div className="header-title-wrp">
+              <Texty
+                className="title"
+                type="mask-top"
+                delay={400}
+                enter={getEnter}
+                interval={geInterval}
+                component={TweenOne}
+                componentProps={{
+                  animation: [
+                    { x: 130, type: 'set' },
+                    { x: 100, delay: 500, duration: 450 },
+                    {
+                      duration: 300,
+                      ease: 'easeOutQuart',
+                      x: 0
+                    },
+                    {
+                      delay: -300,
+                      duration: 1000,
+                      ease: 'easeInOutQuint',
+                      letterSpacing: 0,
+                      scale: 0.9
+                    },
+                    {
+                      delay: -300,
+                      duration: 1000,
+                      ease: 'easeInOutQuint',
+                      scale: 1,
+                      width: '100%'
+                    }
+                  ]
+                }}>
+                Daniel's resume
+              </Texty>
+            </div>
+            <div className="header-menu-wrp">
+              {props.headerMenu.map(item => (
                 <div key={item.title} className="header-title-item">
                   <Link to={item.url}>
                     <span
@@ -159,29 +112,13 @@ const HeaderDom = (props: IProps) => {
                   </Link>
                 </div>
               ))}
-            </Col>
-            <Col
-              xs={24}
-              sm={0}
-              md={{ span: 6, offset: 1 }}
-              xl={{ span: 6, offset: 1 }}
-              xxl={{ span: 6, offset: 1 }}>
-              <Search
-                placeholder="输入搜索标题"
-                onSearch={value => {
-                  props.fetchArticle({
-                    pageIndex: 1,
-                    pageSize: 10,
-                    title: value
-                  })
-                }}
-                className="search-input"
-              />
-            </Col>
-          </Row>
-        </Col>
-        <Col lg={1} xl={4} xxl={5} />
-      </Row>
+              <Dropdown overlay={menu} trigger={['click']}>
+                <Icon type="bars" className="menu-button" />
+              </Dropdown>
+            </div>
+          </Col>
+        </Row>
+      </Header>
     </div>
   )
 }
